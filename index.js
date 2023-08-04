281b03c360647e96cb88b7a25fb5322d51b67434
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2023-08-04 22:04:40 +0300

    Feature/destroy (#7)
    * Добавляет `destroy()`
    
    * Билдит
    
    * Обновляет CHANGELOG.md
    
    * Обновляет README.md
    
    * Исправляет по eslint

--- index.js
+++ index.js
@@ -1,37 +1,60 @@
-const attributeName = 'localstorage';
+class InputSave {
+  #attributeName = 'localstorage';
+  #fields = document.querySelectorAll(`[data-${this.#attributeName}]`);
+  #inputListeners = new Map();
 
-const saveFormData = (field) => {
-  const inputId = field.dataset[attributeName];
-  const value = field.value;
-
-  localStorage.setItem(`${inputId}`, value);
-};
-
-const restoreFormData = (field) => {
-  const inputId = field.dataset[attributeName];
-  const savedValue = localStorage.getItem(inputId);
-
-  if (savedValue !== null) {
-    field.value = savedValue;
+  constructor() {
+    this.init();
   }
-};
 
-const initInputSave = () => {
-  const fields = document.querySelectorAll(`[data-${attributeName}]`);
+  #saveFormData = (field) => {
+    const inputId = field.dataset[this.#attributeName];
+    const value = field.value;
+    localStorage.setItem(`${inputId}`, value);
+  };
 
-  if (fields.length) {
-    for (const field of fields) {
-      field.addEventListener('input', () => {
-        saveFormData(field);
-      });
+  #restoreFormData = (field) => {
+    const inputId = field.dataset[this.#attributeName];
+    const savedValue = localStorage.getItem(inputId);
+
+    if (savedValue !== null) {
+      field.value = savedValue;
     }
+  };
 
-    window.addEventListener('DOMContentLoaded', () => {
-      for (const field of fields) {
-        restoreFormData(field);
-      }
-    });
+  #resetFieldValues = (field) => {
+    field.value = '';
+  };
+
+  #removeInputListeners = (field) => {
+    const inputListener = this.#inputListeners.get(field);
+    field.removeEventListener('input', inputListener);
+  };
+
+  #clearLocalStorage = (field) => {
+    const inputId = field.dataset[this.#attributeName];
+    localStorage.removeItem(inputId);
+  };
+
+  destroy() {
+    for (const field of this.#fields) {
+      this.#resetFieldValues(field);
+      this.#removeInputListeners(field);
+      this.#clearLocalStorage(field);
+    }
   }
-};
 
-export {initInputSave};
+  init() {
+    if (this.#fields.length) {
+      for (const field of this.#fields) {
+        const inputListener = this.#saveFormData.bind(this, field);
+        this.#inputListeners.set(field, inputListener);
+        field.addEventListener('input', inputListener);
+
+        this.#restoreFormData(field);
+      }
+    }
+  }
+}
+
+export { InputSave };
